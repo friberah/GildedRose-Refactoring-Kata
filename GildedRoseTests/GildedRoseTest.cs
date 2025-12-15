111c0578ac44d75ed37948ae84f32069e17e80d7
noeliacar <107847979+noeliacar@users.noreply.github.com>
2025-12-15 11:22:02 +0100

    Add sulfuras testcase

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -14,4 +14,17 @@ public class GildedRoseTest
         app.UpdateQuality();
         Assert.Equal("foo", Items[0].Name);
     }
+
+    [Fact]
+    public void SulfurasItem_NeverHasToBeSoldOrDecreasedInQuality()
+    {
+        IList<Item> Items = new List<Item>
+        {
+            new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 80 }
+        };
+        var app = new GildedRose(Items);
+
+        Assert.Equal(10, Items[0].SellIn);
+        Assert.Equal(80, Items[0].Quality);
+    }
 }
