fe61f4657b52e56634426a309cf5ea0afca18e5b
Supermarcel10 <47798787+Supermarcel10@users.noreply.github.com>
2025-12-17 11:48:50 +0000

    - CH

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -22,16 +22,13 @@ public class GildedRose
                     item.Quality = item.Quality - 1;
                 }
             }
-            else
+            else if (item.Quality < 50)
             {
-                if (item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
+                item.Quality = item.Quality + 1;
 
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        UpdateBackstageQuality(item);
-                    }
+                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    UpdateBackstageQuality(item);
                 }
             }
 
@@ -40,25 +37,26 @@ public class GildedRose
                 item.SellIn = item.SellIn - 1;
             }
 
-            if (item.SellIn < 0)
+            if (item.SellIn >= 0)
             {
-                if (item.Name != "Aged Brie")
-                {
-                    if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros"
-                        && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
+                continue;
+            }
 
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        item.Quality = item.Quality - item.Quality;
-                    }
-                }
-                if (item.Name == "Aged Brie")
-                {
-                    UpdateAgedBrieQuality(item);
-                }
+            if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros"
+                && item.Name != "Backstage passes to a TAFKAL80ETC concert"
+                && item.Name != "Aged Brie")
+            {
+                item.Quality = item.Quality - 1;
+            }
+
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                item.Quality = item.Quality - item.Quality;
+            }
+
+            if (item.Name == "Aged Brie")
+            {
+                UpdateAgedBrieQuality(item);
             }
         }
     }
