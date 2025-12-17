8c457fd6f6824ec79e0fbbe5fa81d7ce7343fd3c
friberah <francesc.ribera@thetrainline.com>
2025-12-17 19:11:58 +0100

    Remove redundant condition

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -18,8 +18,8 @@ public class GildedRose
             if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros") item.Quality = item.Quality - 1;
             if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.Quality < 50) item.Quality = item.Quality + 1;
             if (item.Name == "Aged Brie" && item.Quality < 50) item.Quality = item.Quality + 1;
-            if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.Quality < 50 && item.SellIn < 11 && item.Quality < 50) item.Quality = item.Quality + 1;
-            if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.Quality < 50 && item.SellIn < 6 && item.Quality < 50) item.Quality = item.Quality + 1;
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 11 && item.Quality < 50) item.Quality = item.Quality + 1;
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 6 && item.Quality < 50) item.Quality = item.Quality + 1;
             if (item.Name != "Sulfuras, Hand of Ragnaros") item.SellIn = item.SellIn - 1;
             if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Name != "Sulfuras, Hand of Ragnaros" && item.SellIn < 0 && item.Quality > 0 ) item.Quality = item.Quality - 1;
             if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 0) item.Quality = item.Quality - item.Quality;
