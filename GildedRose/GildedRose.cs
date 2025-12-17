7eed284ede6c5e04dfa2f68479dd6a3ba9171ca0
friberah <francesc.ribera@thetrainline.com>
2025-12-17 18:18:54 +0100

    And again...

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -42,22 +42,21 @@ public class GildedRose
                 item.SellIn = item.SellIn - 1;
             }
 
-            if (item.SellIn < 0)
+            if (item.SellIn < 0 && item.Name != "Aged Brie" &&
+                item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Quality > 0 &&
+                item.Name != "Sulfuras, Hand of Ragnaros")
             {
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.Quality = item.Quality - 1;
-                }
+                item.Quality = item.Quality - 1;
+            }
 
-                if (item.Name != "Aged Brie" && item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    item.Quality = item.Quality - item.Quality;
-                }
+            if (item.SellIn < 0 && item.Name != "Aged Brie" && item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                item.Quality = item.Quality - item.Quality;
+            }
 
-                if (item.Name == "Aged Brie" && item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
-                }
+            if (item.SellIn < 0 && item.Name == "Aged Brie" && item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
             }
         }
     }
