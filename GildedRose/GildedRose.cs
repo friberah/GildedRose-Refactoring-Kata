7f48a94cbd6371cbab03c64ed83408421e7c752b
friberah <francesc.ribera@thetrainline.com>
2025-12-17 18:31:23 +0100

    Removing all the curly braces so we can see all the conditiosn one by one. Now we are in a better position to add the new requirements so we can make the skipped tests pass.

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -15,51 +15,14 @@ public class GildedRose
     {
         foreach (var item in _items)
         {
-            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" &&
-                item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
-            {
-                item.Quality = item.Quality - 1;
-            }
-
-            if ((item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert") &&
-                item.Quality < 50)
-                item.Quality = item.Quality + 1;
-
-            if ((item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert") &&
-                item.Quality < 50 && item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 11 &&
-                item.Quality < 50)
-            {
-                item.Quality = item.Quality + 1;
-            }
-
-            if ((item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert") &&
-                item.Quality < 50 && item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 6 &&
-                item.Quality < 50)
-            {
-                item.Quality = item.Quality + 1;
-            }
-
-            if (item.Name != "Sulfuras, Hand of Ragnaros")
-            {
-                item.SellIn = item.SellIn - 1;
-            }
-
-            if (item.SellIn < 0 && item.Name != "Aged Brie" &&
-                item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Quality > 0 &&
-                item.Name != "Sulfuras, Hand of Ragnaros")
-            {
-                item.Quality = item.Quality - 1;
-            }
-
-            if (item.SellIn < 0 && item.Name != "Aged Brie" && item.Name == "Backstage passes to a TAFKAL80ETC concert")
-            {
-                item.Quality = item.Quality - item.Quality;
-            }
-
-            if (item.SellIn < 0 && item.Name == "Aged Brie" && item.Quality < 50)
-            {
-                item.Quality = item.Quality + 1;
-            }
+            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros") item.Quality = item.Quality - 1;
+            if ((item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert") && item.Quality < 50) item.Quality = item.Quality + 1;
+            if ((item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert") && item.Quality < 50 && item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 11 && item.Quality < 50) item.Quality = item.Quality + 1;
+            if ((item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert") && item.Quality < 50 && item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 6 && item.Quality < 50) item.Quality = item.Quality + 1;
+            if (item.Name != "Sulfuras, Hand of Ragnaros") item.SellIn = item.SellIn - 1;
+            if (item.SellIn < 0 && item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros") item.Quality = item.Quality - 1;
+            if (item.SellIn < 0 && item.Name != "Aged Brie" && item.Name == "Backstage passes to a TAFKAL80ETC concert") item.Quality = item.Quality - item.Quality;
+            if (item.SellIn < 0 && item.Name == "Aged Brie" && item.Quality < 50) item.Quality = item.Quality + 1;
         }
     }
 }
