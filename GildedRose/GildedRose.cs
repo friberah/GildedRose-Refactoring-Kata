16a480464e80d87d21d82b8cfa0a1a3f8170eb2a
friberah <francesc.ribera@thetrainline.com>
2025-12-17 18:06:07 +0100

    Combine another if

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -57,12 +57,9 @@ public class GildedRose
                 {
                     if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (item.Quality > 0)
+                        if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
                         {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
+                            item.Quality = item.Quality - 1;
                         }
                     }
                     else
